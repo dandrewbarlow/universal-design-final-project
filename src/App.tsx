516f53030b3f37f6957ed37dda574f0bb79d4b56
Andrew Barlow <dandrewbarlow@gmail.com>
2021-04-08 20:27:09 -0500

    format code

--- src/App.tsx
+++ src/App.tsx
@@ -4,69 +4,70 @@
 // Main App .tsx File
 
 // Imports
-import React from 'react';
-import './App.scss';
-import * as content from './content/content.jsx';
+import React from "react";
+import "./App.scss";
+import * as content from "./content/content.jsx";
 
 // Header JSX
-function Header(){
-    return (
-      <header>
-        <h1><em>The Importance of Universal Design</em></h1>
-      </header>
-)}
+function Header() {
+  return (
+    <header>
+      <h1>
+        <em>The Importance of Universal Design</em>
+      </h1>
+    </header>
+  );
+}
 
 class Content extends React.Component<{}, { content: string }> {
-  constructor(props:any){
+  constructor(props: any) {
     super(props);
     this.state = {
-      content: 'why',
-    }
+      content: "why",
+    };
   }
 
-  renderLink(text:string, contentKey:string) {
+  renderLink(text: string, contentKey: string) {
     return (
-      <li><button onClick={() => this.setState({content:contentKey})}>{text}</button></li>
-    )
+      <li>
+        <button onClick={() => this.setState({ content: contentKey })}>
+          {text}
+        </button>
+      </li>
+    );
   }
 
-  renderContent(key:string) {
+  renderContent(key: string) {
     switch (key) {
-      case 'why':
-        return (<content.Why />);
+      case "why":
+        return <content.Why />;
 
-      case 'tenets':
-        return (<content.Tenets />);
-    
-      case 'practices':
-        return (<content.Practices />);
+      case "tenets":
+        return <content.Tenets />;
+
+      case "practices":
+        return <content.Practices />;
 
       default:
-        return (<content.Why />);
+        return <content.Why />;
     }
   }
 
   render() {
     return (
       <>
-      <nav>
-        <ul>
-          {this.renderLink('Why Universal Design?', 'why')}
-          {this.renderLink('Tenets', 'tenets')}
-          {this.renderLink('Practices', 'practices')}
-        </ul>
-      </nav>
-      {this.renderContent(this.state.content)}
+        <nav>
+          <ul>
+            {this.renderLink("Why Universal Design?", "why")}
+            {this.renderLink("Tenets", "tenets")}
+            {this.renderLink("Practices", "practices")}
+          </ul>
+        </nav>
+        {this.renderContent(this.state.content)}
       </>
     );
   }
-
 }
-
-
-
-
-
 
 function App() {
   return (
