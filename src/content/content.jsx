7d3b02d550bb955f05b2d628ed45127c23c799c3
Andrew Barlow <dandrewbarlow@gmail.com>
2021-04-08 19:45:00 -0500

    style changes

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -42,7 +42,7 @@ export function Practices() {
       <h3>Semantic HTML</h3>
 
       <p>
-        One of the most important ways to make a website universal is also the most straightforward. The HTML5 standard includes accessibility features baked into the default. By following the rules of semantic web design, a designer is already ahead of the curve. By properly tagging content, accessibility tools like screenreaders can display content in a friendly way to impaired users.
+        Some of the most important ways to make a website universal is also the most straightforward. The HTML5 standard includes accessibility features baked into the default. By following the rules of semantic web design, a designer is already ahead of the curve. By properly tagging content, accessibility tools like screenreaders can display content in a friendly way to impaired users.
       </p>
 
       <h4>Images</h4>
@@ -69,7 +69,7 @@ export function Practices() {
       <img src="img/missing-dog.jpg" alt="A very cute puppy"/>
 
       <p>
-        The alt tag's purpose is to provide a visual description of an image for screenreaders to display to the visually impaired. As seen here, it comes with the added benefit of preventing a missing image source from becoming a page-breaking eyesore. The <code>alt</code> tag is <strong><em>not</em></strong> for inserting keywords for attention from search engines. There are better ways of doing so, and putting it inside image tags creates confusion.
+        The <code>alt</code> tag's purpose is to provide a visual description of an image for screenreaders to display to the visually impaired. As seen here, it comes with the added benefit of preventing a missing image source from becoming a page-breaking eyesore. The <code>alt</code> tag is <strong><em>not</em></strong> for inserting keywords for attention from search engines. There are better ways of doing so, and putting it inside image tags creates confusion.
       </p>
 
       <h4>Semantics and Structure</h4>
@@ -83,30 +83,44 @@ export function Practices() {
 
       <p>
         Here is an example of a simple HTML heirarchy that will work for countless websites. Some of the important features include:
-        <ul>
-          <li>Language declaration [line 2] </li>
-          <li>Extensive use of the <code>meta</code> tag [lines 4-11]</li>
-            <ul>
-              <li>
-                This is where search engine optimization keywords and information goes.
-              </li>
-              <li>
-                This keeps the information visible to "web scrapers" that analyze your content, removing the need to place keywords in odd places that confuse people using assistive technology.
-              </li>
-              <li>
-                This tag also is used to access the <code>viewport</code> property. This tag is used to help a website look right, no matter what device accesses it. There are many more things to do in this regard, but this tag is the starting point.
-              </li>
-            </ul>
-          <li>The actual HTML content is laid out simply and in a heirarchichal structure following the importance of the information. </li>
-          <ul>
-            <li>This is what it means to say universal web design is simple. These are considerations a web designer should already be considering when making a page.</li>
-            <li>Putting the content in these descriptive tabs allow smooth reading and navigation for all users, not just the impaired.</li>
-            <li>Avoid using <code>div</code>'s for anything but styling. They say nothing about their content, so they should not be used for content.</li>
-          </ul>
-          <li>Lastly I would like to guarantee that whatever you're doing, there's a tag for that. I used a simple example, but <a href="https://www.w3schools.com/TAGS/default.ASP">the w3schools website has an excellent reference</a> to the multitude of available tags</li>
-          <li></li>
-        </ul>
       </p>
+
+      <ul>
+        <li>Language declaration [line 2] </li>
+
+        <li>Extensive use of the <code>meta</code> tag [lines 4-11]</li>
+
+          <ul>
+            <li>
+              This is where search engine optimization keywords and information goes.
+            </li>
+
+            <li>
+              This keeps the information visible to "web scrapers" that analyze your content, removing the need to place keywords in odd places that confuse people using assistive technology.
+            </li>
+
+            <li>
+              This tag also is used to access the <code>viewport</code> property. This tag is used to help a website look right, no matter what device accesses it. There are many more things to do in this regard, but this tag is the starting point.
+            </li>
+
+          </ul>
+
+        <li>The actual HTML content is laid out simply and in a heirarchichal structure following the importance of the information. </li>
+
+        <ul>
+          <li>This is what it means to say universal web design is simple. These are considerations a web designer should already be considering when making a page.</li>
+
+          <li>Putting the content in these descriptive tabs allow smooth reading and navigation for all users, not just the impaired.</li>
+
+          <li>Avoid using <code>div</code>'s for anything but styling. They say nothing about their content, so they should not be used for content.</li>
+
+        </ul>
+
+        <li>Lastly I would like to guarantee that whatever you're doing, there's a tag for that. I used a simple example, but <a href="https://www.w3schools.com/TAGS/default.ASP">the w3schools website has an excellent reference</a> to the multitude of available tags</li>
+
+        <li></li>
+
+      </ul>
 
     </section>
   )
