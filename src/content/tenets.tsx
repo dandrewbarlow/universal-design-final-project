e3c1ede6839549cf864bb6c2e302061fb4b07c8f
Andrew Barlow <dandrewbarlow@gmail.com>
2021-04-21 22:06:56 -0600

    added content

--- src/content/tenets.tsx
+++ src/content/tenets.tsx
@@ -7,10 +7,42 @@
 export default function Tenets() {
     return (
         <section>
-        <h2>Tenets of Universal Design</h2>
-        <p>
-            Universal Design posits that the idea of "accessible design" falls short of the goalpost. Accessibility should not be an afterthought or specialty role of a developer, but should be a goal from the starting point of a website's design. It's main point being that accessible design is <em>good</em> design.
-        </p>
+            <h2>Tenets of Universal Design</h2>
+            <p>
+                Universal Design posits that the idea of "accessible design" falls short of the goalpost. Accessibility should not be an afterthought or specialty role of a developer, but should be a goal from the starting point of a website's design. It's main point being that accessible design is <em>good</em> design.
+            </p>
+
+            <p>
+                As a universal designer, one should consider all the different obstacles that a user could encounter, and address them before they're a problem. Designing with adequate color contrast allows visually impaired users to clearly see the components. It does the same for normal users. Providing descriptive labels to images and interactive elements assists those using screen-readers. It also provides context when a resource is unavailable.
+            </p>
+           <p>
+                The point being that providing accessibility resources bolsters a website's usability for everyone, and should be considered a best practice, not an extra feature.
+            </p>
+
+            <p>
+                Universal design can allow equitable internet use for users with varying degrees of ability. The following are some of the impairments that a designer should be actively thinking to address in their design:
+            </p>
+
+            <ul>
+                <li>Auditory</li>
+                <li>Cognitive, learning, &amp; neurological</li>
+                <li>Physical</li>
+                <li>Speech</li>
+                <li>Visual</li>
+            </ul>
+ 
+            <p>Here are some of the principles that define universal design:</p>
+
+            <h3>Tenets</h3>
+            <ul>
+                <li>Equitable use</li>
+                <li>Flexibility in use</li>
+                <li>Simple Intuitive use</li>
+                <li>Perceptible information</li>
+                <li>Tolerance for error</li>
+                <li>Size and space for approach and use</li>
+            </ul>
+
         </section>
     );
 }
